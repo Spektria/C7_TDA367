2465ff78028035de0ed4bf50f6b8be765232e502
Elias Ersson <32256443+Eliasers@users.noreply.github.com>
2021-09-07 16:01:29 +0200

    Replaced main class with JavaFX sample

--- src/main/java/C7Paint.java
+++ src/main/java/C7Paint.java
@@ -1,8 +1,26 @@
 /***
  * C7Paint simply initializes the application.
  */
-public class C7Paint {
-    public static void main(String[] args){
+package sample;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class C7Paint extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
