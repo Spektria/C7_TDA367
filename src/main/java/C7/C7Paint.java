39840d1246dfed32dd6e7904f0dac5b0b541aa4a
Elias Ersson <32256443+Eliasers@users.noreply.github.com>
2021-09-09 14:26:03 +0200

    Removed javafx template from master

--- src/main/java/C7/C7Paint.java
+++ src/main/java/C7/C7Paint.java
@@ -2,27 +2,12 @@ package C7; /***
  * C7.C7Paint simply initializes the application.
  */
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.util.Objects;
 
-public class C7Paint extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        var a = getClass().getResource("sample.fxml");
-        Parent root = FXMLLoader.load(getClass().getResource("../sample.fxml"));
-        primaryStage.setTitle("PaintQlon");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+public class C7Paint {
 
 
     public static void main(String[] args) {
-        launch(args);
+
     }
 }
