7f5733ec679f22ac310a928cebc07baa5a247cf4
Roánie <badlands.hugo@gmail.com>
2021-09-15 20:44:45 +0200

    Updated interfaces ITool and ILayers to new design.

--- src/main/java/C7/ISurface.java
+++
@@ -1,8 +0,0 @@
-package C7;
-
-public interface ISurface {
-    public Color getPixel(int x, int y);
-    public void setPixel(int x, int y, Color color);
-    public int getHeight();
-    public int getWidth();
-}
--- src/main/java/C7/Model/Tools/ITool.java
+++ src/main/java/C7/Model/Tools/ITool.java
@@ -1,17 +1,14 @@
 package C7.Model.Tools;
 
-import C7.ISurface;
+import C7.ILayer;
+import C7.Model.Vector.Vector2D;
 
 /**
- * An ITool performs actions on a {@link ISurface}'s pixels.
+ * An ITool performs actions on a {@link ILayer}'s pixels.
  */
 public interface ITool {
 
-    /**
-     * Performs an action on the given coordinates on the given 2d surface.
-     * @param x the x coordinate in the 2d plane
-     * @param y the y coordinate in the 2d plane
-     * @param surface the plane which is being worked on.
-     */
-    void draw(int x, int y, ISurface surface);
+    void beginDraw(Vector2D pos);
+    void move(Vector2D pos);
+    void endDraw(Vector2D pos);
 }
