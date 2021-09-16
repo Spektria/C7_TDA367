6d40197230c4789c74f9db5803ac1a423df77022
Elias Ersson <32256443+Eliasers@users.noreply.github.com>
2021-09-16 14:30:09 +0200

    Fixed view updating method

--- src/main/java/C7/View/C7PaintView.java
+++ src/main/java/C7/View/C7PaintView.java
@@ -3,6 +3,7 @@ package C7.View;
 import C7.Layer.ILayer;
 import C7.Layer.Layer;
 import C7.Model.Tools.ITool;
+import C7.Model.Tools.PixelPen;
 import C7.Model.Tools.ToolFactory;
 import C7.Model.Vector.Vector2D;
 import C7.Project;
@@ -10,6 +11,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.image.PixelWriter;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -35,29 +37,28 @@ public class C7PaintView implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         gc = canvas.getGraphicsContext2D();
 
-        layer = new Layer(700, 500, new C7.Color(1, 1, 1, 0));
+        layer = new Layer(700, 500, new C7.Color(0, 1, 0, 1));
 
-        currentTool = ToolFactory.CreateCircularBrush(layer, 1, new C7.Color(1, 0, 0, 1));
+        currentTool = ToolFactory.CreateCircularBrush(layer, 5, new C7.Color(1, 0, 0, 1));
+        //currentTool = new PixelPen(layer);
 
         canvasPane.setOnMouseDragged(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 //Move stuff like this to the controller later
                 currentTool.move(new Vector2D(event.getX(), event.getY()));
-                //Arbitrary
-                int updateMargin = 50;
-                updateView(((int)event.getX() - updateMargin), ((int)event.getX() + updateMargin), ((int)event.getY() - updateMargin), ((int)event.getY() + updateMargin));
-                //Code for updating the entire canvas, much slower but seems to give the same result
-                //updateView(0, 0, layer.getWidth(), layer.getHeight());
+
+                updateView();
             }
         });
 
         canvasPane.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                System.out.println("click");
                 if (event.getButton() == MouseButton.PRIMARY) {
                     currentTool.beginDraw(new Vector2D(event.getX(), event.getY()));
+
+                    updateView();
                 }
             }
         });
@@ -75,19 +76,28 @@ public class C7PaintView implements Initializable {
         for (int i = 0; i < 20; i++) {
             ToolButton button = new ToolButton();
             flowPaneTools.getChildren().add(button);
-            System.out.println(button.getLayoutX());
         }
     }
 
-    void updateView(int x, int y, int x1, int y1) {
-        for (int i = 0; i < y1; i++) {
-            for (int j = 0; j < x1; j++) {
+    void updateView(int x, int y, int width, int height) {
+        PixelWriter pw = gc.getPixelWriter();
+        for (int i = 0; i < height*2; i++) {
+            for (int j = 0; j < width*2; j++) {
                 C7.Color color = layer.getPixel(x+j, y+i);
                 if (color == null) continue;
-                System.out.println(x+j);
-                gc.setFill(new Color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()));
                 //Update to take into account canvas transform
-                gc.fillRect(x+j, y+i, x+j, y+i);
+                pw.setColor((x+j-width), (y+i-height), new Color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()));
+            }
+        }
+    }
+
+    void updateView() {
+        PixelWriter pw = gc.getPixelWriter();
+        for (int i = 0; i < layer.getHeight(); i++) {
+            for (int j = 0; j < layer.getWidth(); j++) {
+                C7.Color color = layer.getPixel(j, i);
+                //Update to take into account canvas transform
+                pw.setColor((j), (i), new Color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()));
             }
         }
     }
