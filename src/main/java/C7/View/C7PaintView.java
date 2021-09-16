46ba9ba2a9277745dfb3027e86bb03358aaf68c9
Elias Ersson <eliasersson@hotmail.com>
2021-09-16 11:19:25 +0200

    Implemented tools into the visual test application
    Something is seriously wrong with the code either in the tools or in the view, probably both

--- src/main/java/C7/View/C7PaintView.java
+++ src/main/java/C7/View/C7PaintView.java
@@ -1,5 +1,10 @@
 package C7.View;
 
+import C7.Layer.ILayer;
+import C7.Layer.Layer;
+import C7.Model.Tools.ITool;
+import C7.Model.Tools.ToolFactory;
+import C7.Model.Vector.Vector2D;
 import C7.Project;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -23,36 +28,36 @@ public class C7PaintView implements Initializable {
     @FXML FlowPane flowPaneTools;
     @FXML FlowPane flowPaneProperties;
 
-    boolean primaryPressed;
-    boolean secondaryPressed;
+    ILayer layer; //Only one for now
 
-    Project currentProject;
+    ITool currentTool;
 
     public void initialize(URL location, ResourceBundle resources) {
         gc = canvas.getGraphicsContext2D();
 
+        layer = new Layer(700, 500, new C7.Color(1, 1, 1, 0));
+
+        currentTool = ToolFactory.CreateCircularBrush(layer, 1, new C7.Color(1, 0, 0, 1));
+
         canvasPane.setOnMouseDragged(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                if (primaryPressed) {
-                    useTool(event.getX(), event.getY(), 0);
-                } else if (secondaryPressed) {
-                    useTool(event.getX(), event.getY(), 1);
-                }
+                //Move stuff like this to the controller later
+                currentTool.move(new Vector2D(event.getX(), event.getY()));
+                //Arbitrary
+                int updateMargin = 50;
+                updateView(((int)event.getX() - updateMargin), ((int)event.getX() + updateMargin), ((int)event.getY() - updateMargin), ((int)event.getY() + updateMargin));
+                //Code for updating the entire canvas, much slower but seems to give the same result
+                //updateView(0, 0, layer.getWidth(), layer.getHeight());
             }
         });
 
         canvasPane.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                System.out.println("click");
                 if (event.getButton() == MouseButton.PRIMARY) {
-                    secondaryPressed = false;
-                    primaryPressed = true;
-                    useTool(event.getX(), event.getY(), 0);
-                } else if (event.getButton() == MouseButton.SECONDARY) {
-                    primaryPressed = false;
-                    secondaryPressed = true;
-                    useTool(event.getX(), event.getY(), 1);
+                    currentTool.beginDraw(new Vector2D(event.getX(), event.getY()));
                 }
             }
         });
@@ -61,9 +66,7 @@ public class C7PaintView implements Initializable {
             @Override
             public void handle(MouseEvent event) {
                 if (event.getButton() == MouseButton.PRIMARY) {
-                    primaryPressed = false;
-                }  else if (event.getButton() == MouseButton.SECONDARY) {
-                    secondaryPressed = false;
+                    currentTool.endDraw(new Vector2D(event.getX(), event.getY()));
                 }
             }
         });
@@ -76,15 +79,13 @@ public class C7PaintView implements Initializable {
         }
     }
 
-    public void useTool(double x, double y, int button) {
-        //Wait for tool interface
-    }
-
     void updateView(int x, int y, int x1, int y1) {
         for (int i = 0; i < y1; i++) {
             for (int j = 0; j < x1; j++) {
-                C7.Color color = currentProject.getPixel(x+j, x+i);
-                gc.setFill(new Color(color.getRed()*255, color.getGreen()*255, color.getBlue()*255, color.getAlpha()*255));
+                C7.Color color = layer.getPixel(x+j, y+i);
+                if (color == null) continue;
+                System.out.println(x+j);
+                gc.setFill(new Color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()));
                 //Update to take into account canvas transform
                 gc.fillRect(x+j, y+i, x+j, y+i);
             }
