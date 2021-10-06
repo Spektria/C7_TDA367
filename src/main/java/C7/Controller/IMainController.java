6ff233679e818339cabc599383b6cb1390dedfc8
Roánie <badlands.hugo@gmail.com>
2021-10-06 18:32:22 +0200

    Added JDoc

--- src/main/java/C7/Controller/IMainController.java
+++ src/main/java/C7/Controller/IMainController.java
@@ -2,8 +2,16 @@ package C7.Controller;
 
 import C7.Model.Tools.ITool;
 
+/**
+ * Interface for the primary controller.
+ * @author Hugo Ekstrand
+ */
 public interface IMainController {
 
+    /**
+     * Sets the selected tool to a new, given one.
+     * @param tool the tool which will be selected.
+     */
     void setCurrentTool(ITool tool);
 
 }
