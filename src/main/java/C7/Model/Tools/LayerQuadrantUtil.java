d7595ff39cd0d79271c8c797becc872f88ab9ab4
Roánie <badlands.hugo@gmail.com>
2021-10-10 16:17:19 +0200

    Added jdoc to LayerQuadrantUtil

--- src/main/java/C7/Model/Tools/LayerQuadrantUtil.java
+++ src/main/java/C7/Model/Tools/LayerQuadrantUtil.java
@@ -4,8 +4,19 @@ import C7.Model.Layer.ILayer;
 import C7.Util.Vector2D;
 
 final class LayerQuadrantUtil {
-    static Vector2D getQuadrantAsVector(ILayer layer, Vector2D position){
-        Vector2D center = layer.getPosition().add(new Vector2D(layer.getWidth()/2d, layer.getHeight()/2d));
+    /**
+     * Returns a given points position relative to a given layer's global quadrants.
+     * That is, the layer is considered as if it is not rotated.
+     * The quadrant is represented as a vector so that
+     * the vectors listed are in the order of the quadrants:
+     * [1,1], [-1,1], [-1,-1], [-1,-1].
+     * The output is guaranteed to be one of these vectors.
+     * @param layer the given layer
+     * @param position the given point
+     * @return the quadrant the point is in.
+     */
+    static Vector2D getGlobalQuadrantPosition(ILayer layer, Vector2D position){
+        Vector2D center = layer.getPosition().add(layer.getLocalCenterPoint());
         return new Vector2D(
                 center.getX() < position.getX() ? 1 : -1,
                 center.getY() < position.getY() ? 1 : -1
