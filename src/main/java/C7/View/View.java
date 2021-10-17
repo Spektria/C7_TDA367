e253db01e8e3137c383fb5f025347fb5c7bb12ae
Roánie <badlands.hugo@gmail.com>
2021-10-17 20:36:14 +0200

    Simplified method via C7Math util. Also fixed width & height property not being used.

--- src/main/java/C7/View/View.java
+++ src/main/java/C7/View/View.java
@@ -1,5 +1,6 @@
 package C7.View;
 
+import C7.Util.C7Math;
 import C7.Util.IObserver;
 import C7.Model.IProject;
 import C7.Util.Tuple2;
@@ -90,22 +91,25 @@ class View implements IView, IObserver<Tuple2<Vector2D, Vector2D>> {
     @Override
     public void notify(Tuple2<Vector2D, Vector2D> data) {
 
+
+        // Render only what is in the canvas. That is, any value under 0 or above the view's width
+        // should be ignored.
         render(
-                (int)Math.max(data.getVal1().getX(), 0),
-                (int)Math.max(data.getVal1().getY(), 0),
-                (int)Math.min(Math.max(data.getVal2().getX() + 1, 0), project.getWidth()),
-                (int)Math.min(Math.max(data.getVal2().getY() + 1, 0), project.getHeight())
+                C7Math.limit(data.getVal1().getX(), 0d, width.get()).intValue(),
+                C7Math.limit(data.getVal1().getY(), 0d, height.get()).intValue(),
+                C7Math.limit(data.getVal2().getX() + 1, 0d, width.get()).intValue(),
+                C7Math.limit(data.getVal2().getY() + 1, 0d, height.get()).intValue()
         );
 
         // We update the last rectangle too, so that no leftovers are visible on the view.
         // That is, if we were not to do this ghost images may be left on the view.
         if(lastUpdateRect != null)
             render(
-                    (int)Math.max(lastUpdateRect.getVal1().getX(), 0),
-                    (int)Math.max(lastUpdateRect.getVal1().getY(), 0),
-                    (int)Math.min(Math.max(lastUpdateRect.getVal2().getX() + 1, 0), project.getWidth()),
-                    (int)Math.min(Math.max(lastUpdateRect.getVal2().getY() + 1, 0), project.getHeight())
-            );
+                    C7Math.limit(lastUpdateRect.getVal1().getX(), 0d, width.get()).intValue(),
+                    C7Math.limit(lastUpdateRect.getVal1().getY(), 0d, height.get()).intValue(),
+                    C7Math.limit(lastUpdateRect.getVal2().getX() + 1d, 0d, width.get()).intValue(),
+                    C7Math.limit(lastUpdateRect.getVal2().getY() + 1d, 0d, height.get()).intValue()
+                    );
 
         this.lastUpdateRect = data;
     }
